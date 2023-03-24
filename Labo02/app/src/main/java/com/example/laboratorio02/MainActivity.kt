ab4764fdc96a08e93f475808d7827b6fc609090a
cam-vasquez <00065221@uca.edu.sv>
2023-03-24 13:23:56 -0600

    :sparkles: update labo

--- Labo02/app/src/main/java/com/example/laboratorio02/MainActivity.kt
+++ Labo02/app/src/main/java/com/example/laboratorio02/MainActivity.kt
@@ -3,8 +3,14 @@ package com.example.laboratorio02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 private lateinit var calculateButton: Button
+private lateinit var weightTV: EditText
+private lateinit var heightTV: EditText
+private lateinit var rangeTV: EditText
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +18,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //bind()
             // ocultando el actionBar
-        supportActionBar!!.hide();
+        //supportActionBar!!.hide();
 
 
+    }
+
+    private fun setListener() {
+        TODO("Not yet implemented")
     }
 }
 

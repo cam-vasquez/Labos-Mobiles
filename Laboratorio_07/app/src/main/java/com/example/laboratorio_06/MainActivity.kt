a34e709978d65166ccd0cf0ecfd93990f9b04f3e
Fernanda Vásquez <82715400+cam-vasquez@users.noreply.github.com>
2023-05-04 12:50:31 -0600

    Update MainActivity.kt

--- Laboratorio_07/app/src/main/java/com/example/laboratorio_06/MainActivity.kt
+++ Laboratorio_07/app/src/main/java/com/example/laboratorio_06/MainActivity.kt
@@ -13,9 +13,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding = DataBindingUtil
-            .setContentView(this, R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 
 }

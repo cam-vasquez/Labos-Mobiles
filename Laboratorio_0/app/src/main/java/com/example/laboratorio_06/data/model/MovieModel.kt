11951cc7bd12d1571bc50d728beeee24e7f1e7ca
cam-vasquez <00065221@uca.edu.sv>
2023-06-11 20:38:43 -0600

    :tada: Add Labo11

--- Laboratorio_0/app/src/main/java/com/example/laboratorio_06/data/model/MovieModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.laboratorio_06.data.model
-
-data class MovieModel(
-    val name: String,
-    val category: String,
-    val description: String,
-    val qualification: String,
-)
--- Laboratorio_0/app/src/main/java/com/example/laboratorio_06/repositories/MovieRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.laboratorio_06.repositories
-
-import com.example.laboratorio_06.data.model.MovieModel
-
-class MovieRepository (private  val movies: MutableList<MovieModel>){
-    fun getMovies ()= movies
-    fun setMovies (movie: MovieModel) = movies.add(movie)
-}
